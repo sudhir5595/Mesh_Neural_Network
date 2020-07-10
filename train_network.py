12ddc6a8d7a7f2d7260f73a3bb2b2b47df9ffd5c
Shalabh Gupta <shalabh147@gmail.com>
2020-07-10 12:58:47 +0530

    Update train_network.py

--- train_network.py
+++ train_network.py
@@ -12,15 +12,20 @@ optimizer = optim.SGD(model.parameters(),lr = 0.001,momentum = 0.9)
 
 from dataset import MeshData
 DataObject = MeshData('/home/prathmesh/Desktop/SoC-2020/ModelNet10_stl/ModelNet10')
+dataLoad = torch.utils.data.DataLoader(DataObject, batch_size=1, shuffle=True)
+batch = next(iter(dataLoad))
+print(len(batch))
 
 max_epochs = 30
+loss_list = []
 for epochs in range(max_epochs):
 	#print('e =',epochs)
 	running_loss = 0.0
-	for i in range(len(DataObject.X)):
-		x,y = DataObject[i]
-		x = x.float()
+	for i,data in enumerate(dataLoad,0):
+		x,y = data
+		x = x[0].float().to(device)
 		#y = y.float()
+		y = y[0].to(device)
 		optimizer.zero_grad()
 		yhat = model(x)
 		loss = criterion(yhat,y)
@@ -32,6 +37,16 @@ for epochs in range(max_epochs):
 			print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 200))
 			running_loss = 0.0
 	#print(running_loss)
+	import matplotlib.pyplot as plt
+    	plt.plot(loss_list)
+    	plt.title('Model_loss vs epochs')
+    	plt.ylabel('Loss')
+    	plt.xlabel('epochs')
+    	s = '../working/epochwise_loss_' + str(epochs)
+    	plt.savefig(s)
+    	plt.show()
+    	plt.close()
+    	torch.save(model.state_dict(), '../working/new_mod.pth')
 
 PATH = '/home/prathmesh/Desktop/Mesh_Neural_Network/new_models.pth'
 torch.save(model.state_dict(), PATH)
